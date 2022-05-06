2298fe6c383e2b3fd124d6016bd15d81ae519719
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-06 19:06:49 +0300

    chore: set paths

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import WelcomeMessage from 'src/components/WelcomeMessage';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <WelcomeMessage />
       <StatusBar style="auto" />
     </View>
   );
