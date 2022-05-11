a5ccbc6502285a853cb736bb227635b4767dfa89
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-11 16:50:02 +0300

    test(search-input): add tests

--- src/components/SearchInput/SearchInput.test.tsx
+++ src/components/SearchInput/SearchInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 
 import SearchInput from './SearchInput';
 
@@ -6,14 +6,51 @@ describe('SearchInput', () => {
   const mockPlaceholder = 'Type here';
   const mockOnSubmit = jest.fn();
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders successfully', () => {
     const { toJSON } = render(
       <SearchInput
         placeholder={mockPlaceholder}
-        onSubmit={() => {}}
+        onSubmit={mockOnSubmit}
       />
     );
 
     expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('submits the entered search term', () => {
+    const { getByPlaceholderText } = render(
+      <SearchInput
+        placeholder={mockPlaceholder}
+        onSubmit={mockOnSubmit}
+      />
+    );
+
+    const textInput = getByPlaceholderText(mockPlaceholder);
+
+    fireEvent.changeText(textInput, 'Kyiv');
+
+    fireEvent(textInput, 'submitEditing');
+
+    expect(mockOnSubmit).toHaveBeenCalledTimes(1);
+    expect(mockOnSubmit).toHaveBeenCalledWith('Kyiv');
+  });
+
+  it('does not calls the onSubmit callback if the search term is empty', () => {
+    const { getByPlaceholderText } = render(
+      <SearchInput
+        placeholder={mockPlaceholder}
+        onSubmit={mockOnSubmit}
+      />
+    );
+
+    const textInput = getByPlaceholderText(mockPlaceholder);
+
+    fireEvent(textInput, 'submitEditing');
+
+    expect(mockOnSubmit).not.toHaveBeenCalled();
   });
 });
